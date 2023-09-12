6bd0cbd612a5ab783a0e21fac5548993977d1c4b
Anisha Ghoshal <67908707+ANISHAGHOSHAL@users.noreply.github.com>
2023-09-12 20:13:24 +0530

    Time: 69 ms (46.63%), Space: 17.5 MB (22.39%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int minDeletions(string s) {
-        // Store the frequency of each character
-        vector<int> frequency(26, 0);
+    int minDeletions(std::string s) {
+        std::unordered_map<char, int> cnt;
+        int deletions = 0;
+        std::unordered_set<int> used_frequencies;
+        
         for (char c : s) {
-            frequency[c - 'a']++;
+            cnt[c]++;
         }
         
-        int deleteCount = 0;
-        // Use a set to store the frequencies we have already seen
-        unordered_set<int> seenFrequencies;
-        for (int i = 0; i < 26; i++) {
-            // Keep decrementing the frequency until it is unique
-            while (frequency[i] && seenFrequencies.find(frequency[i]) != seenFrequencies.end()) {
-                frequency[i]--;
-                deleteCount++;
+        for (auto& kv : cnt) {
+            int freq = kv.second;
+            while (freq > 0 && used_frequencies.find(freq) != used_frequencies.end()) {
+                freq--;
+                deletions++;
             }
-            // Add the newly occupied frequency to the set
-            seenFrequencies.insert(frequency[i]);
+            used_frequencies.insert(freq);
         }
         
-        return deleteCount;
+        return deletions;
     }
 };
